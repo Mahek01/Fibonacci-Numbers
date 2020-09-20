669a031adf8d7affd9d7e72b23ad2b4e7bafc3c0
Mahek01 <69900882+Mahek01@users.noreply.github.com>
2020-09-20 11:26:01 +0530

    Fibonacci Numbers
    This python code gives the desired number of fibonacci series.

---
+++ Fibonacci Series.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the value of 'n': "))
+a = 0
+b = 1
+print(a)
+print(b)
+for i in range(0,n):
+    c=a+b
+    a=b
+    b=c
+    print(a+b)
+#the above was done in order to swap.
+
